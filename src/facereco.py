d5950ba52879b7c3fae1434f6a39ab279859120b
Yihui Guo <vidaguoyh@hotmail.com>
2018-06-21 05:28:32 +0800

    Add blacklist detection, add load/generate feature file

--- src/facereco.py
+++ src/facereco.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 import face_recognition
+import pickle
 
 class face_reco:
     def __init__(self):
@@ -8,7 +9,15 @@ class face_reco:
         self.knownNames=[]
 
     def init_with_images(self,folderpath):
-        for parent,dirnames,filenames in os.walk(folderpath):
+        if(os.path.isfile(folderpath+"\\bak_face.json") and os.path.isfile(folderpath+"\\bak_name.json")):
+                print("predifined model found, skipping loading images...")
+                facef = open(folderpath+"\\bak_face.json","rb")
+                namef = open(folderpath+"\\bak_name.json","rb")
+                self.knownFaces = pickle.loads(facef.read())
+                self.knownNames = pickle.loads(namef.read())
+                print(len(self.knownNames))
+                return
+        for parent,dirnames,filenames in os.walk(folderpath):          
             print("Loading known images")
             for filename in filenames:
                 file_path = os.path.join(parent,filename)
@@ -16,10 +25,14 @@ class face_reco:
                     img = face_recognition.load_image_file(file_path)
                     encoding = face_recognition.face_encodings(img)[0]
                     name = file_path.split('\\')[-1][0:-4]
-                    #print (name)
-
+                    print (name)
+                    
                     self.knownFaces.append(encoding)
                     self.knownNames.append(name)
+        facef = open(folderpath+"\\bak_face.json","wb")
+        namef = open(folderpath+"\\bak_name.json","wb")
+        facef.write(pickle.dumps(self.knownFaces))
+        namef.write(pickle.dumps(self.knownNames))
         print("Ready to go!")
 
     def process_one_pic(self,frame):
@@ -37,7 +50,19 @@ class face_reco:
                 name = self.knownNames[first_match_index]
                 
             face_names.append(name)
-        return face_locations,face_names  
+        return face_locations,face_names
+
+    def CheckMatch(self,frame):
+        isMatch = False
+        face_locations = face_recognition.face_locations(frame)
+        face_encodings = face_recognition.face_encodings(frame, face_locations)
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(self.knownFaces, face_encoding,tolerance=0.4)       
+            # If a match was found in known_face_encodings, just use the first one.
+            if True in matches:
+                isMatch=True
+        return isMatch
 
     def reset(self):
         self.knownFaces=[]
--- src/wrapper.py
+++ src/wrapper.py
@@ -2,11 +2,15 @@ import cv2
 from facereco import face_reco
 
 rootdir = "D:\\facedemo\\imgs"
+blackListDir = "D:\\facedemo\\blacklist"
 handler = face_reco()
 handler.init_with_images(rootdir)
+blacklist_handler = face_reco()
+blacklist_handler.init_with_images(blackListDir)
 
 video_capture = cv2.VideoCapture(0)
 NoFaceCountDown = -10
+MatchBlackList = False
 process_this_frame = True
 font = cv2.FONT_HERSHEY_DUPLEX
 
@@ -20,9 +24,17 @@ while True:
     # Only process every other frame of video to save time
     if process_this_frame:
         faceCount=0
-        result = handler.process_one_pic(rgb_small_frame)
-        locs = result[0]
-        names=result[1]
+        blacklist = blacklist_handler.CheckMatch(rgb_small_frame)
+        if(blacklist==True):
+            MatchBlackList=True
+            result = blacklist_handler.process_one_pic(rgb_small_frame)
+            locs = result[0]
+            names=result[1]
+        else:
+            MatchBlackList=False
+            result = handler.process_one_pic(rgb_small_frame)
+            locs = result[0]
+            names=result[1]
     process_this_frame = not process_this_frame
     # Display the results
     for (top, right, bottom, left), name in zip(locs, names):
@@ -32,19 +44,27 @@ while True:
         bottom *= 4
         left *= 4
         # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        if(MatchBlackList==False):
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, str(name), (left + 6, bottom - 6), font, 0.8, (0, 0, 0), 1)
+        else:
+            cv2.rectangle(frame, (left, top), (right, bottom), (255, 255, 255), 2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, str(name), (left + 6, bottom - 6), font, 0.8, (0, 0, 0), 1)
         faceCount=faceCount+1
         # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
-        cv2.putText(frame, str(name), (left + 6, bottom - 6), font, 0.8, (0, 0, 0), 1)
-    if(faceCount==0):
-        if(NoFaceCountDown>0):
-            cv2.putText(frame, "Shaded", (0, 30), font, 1.5, (0, 255, 255), 2)
-            NoFaceCountDown=NoFaceCountDown-1
-        else:
-            cv2.putText(frame, "Not Started", (0, 30), font, 1.5, (0, 0, 255), 2)
+    if(MatchBlackList==True):
+        cv2.putText(frame, "Black List", (0, 30), font, 1.5, (0, 0, 0), 2)      
     else:
-        NoFaceCountDown=50
+        if(faceCount==0):
+            if(NoFaceCountDown>0):
+                cv2.putText(frame, "Shaded", (0, 30), font, 1.5, (0, 255, 255), 2)
+                NoFaceCountDown=NoFaceCountDown-1
+            else:
+                cv2.putText(frame, "Not Started", (0, 30), font, 1.5, (0, 0, 255), 2)
+        else:
+            NoFaceCountDown=50
     # Display the resulting image
     cv2.imshow('Video', frame)
     # Hit 'q' on the keyboard to quit
