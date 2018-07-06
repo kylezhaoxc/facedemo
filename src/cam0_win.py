4490c8b8a08643fa7440d85411cbc8a53e769635
Yihui Guo <vidaguoyh@hotmail.com>
2018-07-07 01:07:14 +0800

    add multithread

--- src/cam0_win.py
+++ src/cam0_win.py
@@ -1,14 +1,10 @@
 import cv2
 from facereco import face_reco
 import datetime
-import serial
-import checkSum
+from multiprocessing.pool import ThreadPool
 
-def SendOpenCommand(portName):
-    port = serial.Serial(portName,115200,8)
-    bytearr = bytearray([0x55,0xff,0xff,0x01,0xff,0xff,0x01])    
-    print(bytearr)
-    
+pool = ThreadPool(processes=4)
+
 rootdir = "D:\\facedemo\\imgs"
 blackListDir = "D:\\facedemo\\blacklist"
 handler = face_reco()
@@ -25,18 +21,26 @@ NoFaceCountDown = -10
 MatchBlackList = False
 process_this_frame = True
 font = cv2.FONT_HERSHEY_DUPLEX
-
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-    # Resize frame of video to 1/4 size for faster face recognition processing
+faceCount=0
+top=0
+bottom =0
+left =0
+right=0
+name = ""
+def handleframe(frame):
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
+    locs = []
+    names=[]
+    
     # Only process every other frame of video to save time
+    global process_this_frame
+    global faceCount
     if process_this_frame:
         faceCount=0
         blacklist = blacklist_handler.process_one_pic(rgb_small_frame)
+        global MatchBlackList
         if(blacklist[2]==True):
             MatchBlackList=True
             locs = blacklist[0]
@@ -48,6 +52,11 @@ while True:
             names=result[1]
     process_this_frame = not process_this_frame
     # Display the results
+    global top
+    global right 
+    global bottom
+    global left
+    global name
     for (top, right, bottom, left), name in zip(locs, names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 4
@@ -55,20 +64,13 @@ while True:
         bottom *= 4
         left *= 4
         # Draw a box around the face
-        if(MatchBlackList==False):
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
-            cv2.putText(frame, str(name), (left + 6, bottom - 6), font, 0.8, (0, 0, 0), 1)
-        else:
-            cv2.rectangle(frame, (left, top), (right, bottom), (255, 255, 255), 2)
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (255, 255, 255), cv2.FILLED)
-            cv2.putText(frame, str(name), (left + 6, bottom - 6), font, 0.8, (0, 0, 0), 1)
         faceCount=faceCount+1
         # Draw a label with a name below the face
     if(MatchBlackList==True):
         cv2.putText(frame, "Black List", (0, 30), font, 1.5, (0, 0, 0), 2)      
     else:
         if(faceCount==0):
+            global NoFaceCountDown
             if(NoFaceCountDown>0):
                 cv2.putText(frame, "Shaded", (0, 30), font, 1.5, (0, 255, 255), 2)
                 NoFaceCountDown=NoFaceCountDown-1
@@ -76,9 +78,25 @@ while True:
                 cv2.putText(frame, "Not Started", (0, 30), font, 1.5, (0, 0, 255), 2)
         else:
             NoFaceCountDown=50
+    return frame
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    async_result= pool.apply_async(handleframe,(frame,))
+    outframe = async_result.get()
+    if(MatchBlackList==False):
+        cv2.rectangle(outframe, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.rectangle(outframe, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        cv2.putText(outframe, str(name), (left + 6, bottom - 6), font, 0.8, (0, 0, 0), 1)
+    else:
+        cv2.rectangle(outframe, (left, top), (right, bottom), (255, 255, 255), 2)
+        cv2.rectangle(outframe, (left, bottom - 35), (right, bottom), (255, 255, 255), cv2.FILLED)
+        cv2.putText(outframe, str(name), (left + 6, bottom - 6), font, 0.8, (0, 0, 0), 1)
     # Display the resulting image
-    cv2.imshow('Video', frame)
-    out.write(frame)
+    cv2.imshow('Video', outframe)
+
+    out.write(outframe)
     curr = datetime.datetime.now()
     if (curr-starttime).total_seconds()>3600:
         starttime=curr
